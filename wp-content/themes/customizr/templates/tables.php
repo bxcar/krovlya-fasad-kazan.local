7ba4c39bc70c90d1df99c49cdf501cc513bb5b06
bxcar <malanchukdima@mail.ru>
2018-05-14 15:16:56 +0300

    several changes

---
+++ wp-content/themes/customizr/templates/tables.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Tables
+ */
+get_header(); ?>
+<h2 class="tables-title"><?php the_title(); ?></h2>
+<span class="tables-subtitle">Внимание! Расчет сделан на типовой участок фасада<br>и не учитывает архитектурных особенностей здания</span>
+
+
+<?php get_footer(); ?>

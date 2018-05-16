186ca9b922d28e45acff18f6d0e8590e0ed021cc
bxcar <malanchukdima@mail.ru>
2018-05-16 11:49:57 +0300

    tables html and css done (without adaptation)

--- wp-content/themes/customizr/templates/tables.php
+++ wp-content/themes/customizr/templates/tables.php
@@ -129,6 +129,143 @@ get_header(); ?>
             <td>605.00</td>
         </tr>
     </table>
+
+    <table class="main-table table2">
+        <tr>
+            <th class="mainth" colspan="8">Крепеж</th>
+        </tr>
+        <tr>
+            <th class="subth">№<br>п.п.</th>
+            <th class="subth">Наименование</th>
+            <th class="subth">Артикул</th>
+            <th class="subth">Ед.<br>изм.</th>
+            <th class="subth">Общее<br>кол-во</th>
+            <th class="subth">Кол-во<br>в упак.</th>
+            <th class="subth">Цена с<br>НДС за ед.<br>(руб)</th>
+            <th class="subth">Сумма с<br>НДС (руб)</th>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Заклепка</td>
+            <td>K14 5x12</td>
+            <td>шт</td>
+            <td>2557</td>
+            <td>500</td>
+            <td>3.50</td>
+            <td>8,948.33</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>Заклепка</td>
+            <td>K6 3x8</td>
+            <td>шт</td>
+            <td>4261</td>
+            <td>500</td>
+            <td>2.70</td>
+            <td>11,505.00</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td>Фасадный анкерный дюбель</td>
+            <td>10*100F</td>
+            <td>шт</td>
+            <td>1704</td>
+            <td>300</td>
+            <td>18.20</td>
+            <td>31,020.89</td>
+        </tr>
+        <tr class="bottom-line">
+            <td></td>
+            <td colspan="5" class="first-footer-title">Стоимость крепежа системы "SL-100"</td>
+            <td class="total">Итого:</td>
+            <td>51,474.22</td>
+        </tr>
+        <tr class="bottom-line last-line">
+            <td></td>
+            <td colspan="6" class="first-footer-title">Стоимость крепежа системы "SL-100" на 1 м2</td>
+            <td>67.11</td>
+        </tr>
+    </table>
+
+    <table class="main-table table2">
+        <tr>
+            <th class="mainth" colspan="8">Утеплитель. Анкер. Керамогранит.</th>
+        </tr>
+        <tr>
+            <th class="subth">№<br>п.п.</th>
+            <th class="subth">Наименование</th>
+            <th class="subth">Артикул</th>
+            <th class="subth">Ед.<br>изм.</th>
+            <th class="subth">Общее<br>кол-во</th>
+            <th class="subth">Кол-во<br>в упак.</th>
+            <th class="subth">Цена с<br>НДС за ед.<br>(руб)</th>
+            <th class="subth">Сумма с<br>НДС (руб)</th>
+        </tr>
+        <tr>
+            <td>11</td>
+            <td>Утеплитель</td>
+            <td>0.1</td>
+            <td>м3</td>
+            <td>84</td>
+            <td></td>
+            <td></td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>12</td>
+            <td>Негорючая ткань "TEND KM-O"</td>
+            <td>1.15</td>
+            <td>м2</td>
+            <td>882</td>
+            <td></td>
+            <td></td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>13</td>
+            <td>Анкера для утеплителя</td>
+            <td>7</td>
+            <td>шт</td>
+            <td>5369</td>
+            <td></td>
+            <td></td>
+            <td>0.00</td>
+        </tr>
+        <tr>
+            <td>14</td>
+            <td>Керамогранит серо-бежевый соль-перец</td>
+            <td>1.1</td>
+            <td>м2</td>
+            <td>767</td>
+            <td></td>
+            <td></td>
+            <td>0.00</td>
+        </tr>
+        <tr class="bottom-line">
+            <td></td>
+            <td colspan="5" class="first-footer-title">Стоимость утеплителей системы "SL-100"</td>
+            <td class="total">Итого:</td>
+            <td>0.00</td>
+        </tr>
+        <tr class="bottom-line last-line">
+            <td></td>
+            <td colspan="6" class="first-footer-title">Стоимость утеплителей системы "SL-100" на 1 м2</td>
+            <td>0.00</td>
+        </tr>
+    </table>
+
+    <table class="main-table table2">
+        <tr class="bottom-line">
+            <td colspan="6" class="first-footer-title">Стоимость системы "SL-100"</td>
+            <td></td>
+            <td>Итого:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;515,509.22</td>
+        </tr>
+        <tr class="bottom-line last-line">
+            <td colspan="6" class="first-footer-title">Стоимость системы "SL-100" на 1 м2</td>
+            <td></td>
+            <td>672.11</td>
+        </tr>
+    </table>
 </div>
 
 <?php get_footer(); ?>
